cd8ae49423e8417253bc6f6018c032901c90293f
logicp <adventurist@gmail.com>
2020-01-27 21:59:17 -0500

    adding cron jobs works now

--- include/system/cron.hpp
+++ include/system/cron.hpp
@@ -5,54 +5,118 @@
 
 namespace System {
 
-struct Job {
+  /*
+
+    * * * * *
+    | | | | |
+    | | | | + --- day of week (0 - 6 / Sun - Sat)
+    | | | +------ month (1 - 12)
+    | | +-------- day of month (1 - 31)
+    | +---------- hour (0 - 23)
+    +------------ minute (0 - 59)
+
+
+   */
+
+namespace Month {
+  static constexpr int JANUARY = 0;
+  static constexpr int FEBRUARY = 1;
+  static constexpr int MARCH = 2;
+  static constexpr int APRIL = 3;
+  static constexpr int MAY = 4;
+  static constexpr int JUNE = 5;
+  static constexpr int JULY = 6;
+  static constexpr int AUGUST = 7;
+  static constexpr int SEPTEMBER = 8;
+  static constexpr int OCTOBER = 9;
+  static constexpr int NOVEMBER = 10;
+  static constexpr int DECEMBER = 11;
+}
+
+enum JobType {
+  Single = 0,
+  Daily = 1,
+  Weekly = 2,
+  Monthly = 3,
+  Yearly = 4
+};
+struct WeeklyJob {
   std::string path;
-  bool one_time;
+  int day;
   int hour;
   int minute;
-  int second;
+  JobType type = JobType::Weekly;
+};
+
+struct SingleJob {
+  std::string path;
+  int month;
+  int day_of_month;
+  int hour;
+  int minute;
+  JobType type = JobType::Single;
+};
+
+struct DailyJob {
+  std::string path;
+  int hour;
+  int minute;
+  JobType type = JobType::Daily;
 };
 
 auto KLOG = KLogger::GetInstance()->get_logger();
 
+template <typename Job>
 class CronInterface {
   public:
     virtual std::string listJobs() = 0;
-    virtual void addJob() = 0;
-    virtual void deleteJob() = 0;
+    virtual void addJob(Job job) = 0;
+    virtual void deleteJob(Job job) = 0;
 };
 
-class Cron : public CronInterface {
+template <typename Job>
+class Cron : public CronInterface<Job> {
   public:
     virtual std::string listJobs() override {
+      std::system("crontab -l > cron.txt");
+      return std::string {
+        static_cast<std::stringstream const&>(std::stringstream() <<
+          std::ifstream("cron.txt").rdbuf()
+        ).str()
+      };
+    }
 
-      auto jobs = std::system("crontab -l > cron.txt");
-      return std::string {static_cast<std::stringstream const&>(std::stringstream() << std::ifstream("cron.txt").rdbuf()).str()};
+    std::string createJobCommand(Job job) {
+      if (job.type == JobType::Single) {
+        return std::string{
+          "(crontab -l ; echo \"" +
+          std::to_string(job.minute) + " " +
+          std::to_string(job.hour) + " " +
+          std::to_string(job.day_of_month) + " " +
+          std::to_string(job.month) + " * " +
+          job.path +
+          "\") | sort - | uniq - | crontab -"
+        };
+      }
     }
 
     virtual void addJob(Job job) {
       KLOG->info("Add Job to be implemented");
-      std::system(
-        std::string(
-          "(crontab -l ; echo \"0 * * * * " +
-          job.path +
-          ") | sort - | uniq - | crontab -"
-        ).c_str()
-      );
+      std::system(createJobCommand(job).c_str());
+
       if (findJob(job.path)) {
         KLOG->info("Successfully added job to cron: {}", job.path);
       } else {
-          KLOG->info("Failed to add job to cron: {}", job.path);
+        KLOG->info("Failed to add job to cron: {}", job.path);
       }
     }
 
-    virtual void deleteJob() {
+    virtual void deleteJob(Job job) {
       KLOG->info("Delete Job to be implemented");
     }
 
-    bool findJob(std::string job) {
-      auto jobs = std::string {static_cast<std::stringstream const&>(std::stringstream() << std::ifstream("cron.txt").rdbuf()).str()};
-      return std::string::npos != jobs.find(job);
-  }
+    bool findJob(std::string job_command) {
+      return (listJobs().find(job_command) != std::string::npos);
+    }
 };
 }
