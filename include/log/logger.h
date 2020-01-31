1a0bce757e9a2a8abb08a1dba35ee8a5d6aca157
logicp <adventurist@gmail.com>
2020-01-31 16:10:13 -0500

    Revert "attempting this config for tracing"
    This reverts commit b1f704bce2cb7c8f43e5a54e33aff1be54a00d0e.

--- include/log/logger.h
+++ include/log/logger.h
@@ -1,19 +1,10 @@
 #ifndef __LOGGER_H__
 #define __LOGGER_H__
 
-#define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_TRACE
-#define SPDLOG_DEBUG_ON
-#define SPDLOG_TRACE_ON
-
-#define KLOG SPDLOG_LOGGER_TRACE
-// #define KLOG->trace SPDLOG_TRACE
-
 #include <spdlog/sinks/basic_file_sink.h>
 #include <spdlog/spdlog.h>
-#include <spdlog/sinks/stdout_sinks.h>
-
-
 #include <chrono>
+
 #include <iostream>
 #include <memory>
 
@@ -26,21 +17,14 @@ log_ptr g_logger;
 KLogger* g_instance;
 
 class KLogger {
+
  public:
   KLogger() {
     try {
-      // g_logger = spdlog::basic_logger_mt("KLOG", "/tmp/kserver/k.log");
-      // spdlog::set_level(spdlog::level::info);
-      // spdlog::flush_every(std::chrono::seconds(5));
-      // g_logger->info("Initializing logger");
-      spdlog::set_pattern("[source %s] [function %!] [line %#] %v");
-      auto console = spdlog::basic_logger_mt("KLOG", "/tmp/kserver/k.log");
-
-      spdlog::set_default_logger(console);
-
-      SPDLOG_TRACE("global output with arg {}", 1); // [source main.cpp] [function main] [line 16] global output with arg 1
-      SPDLOG_LOGGER_TRACE(console, "logger output with arg {}", 2); // [source main.cpp] [function main] [line 17] logger output with arg 2
-      console->info("invoke member function"); // [source ] [function ] [line ] invoke member function
+      g_logger = spdlog::basic_logger_mt("KLOG", "/tmp/kserver/k.log");
+      spdlog::set_level(spdlog::level::info);
+      spdlog::flush_every(std::chrono::seconds(5));
+      g_logger->info("Initializing logger");
     } catch (const spdlog::spdlog_ex& ex) {
       std::cout << "Error: " << ex.what() << std::endl;
     }
@@ -57,6 +41,6 @@ class KLogger {
 
   log_ptr static get_logger() { return g_logger; }
 };
-}  // namespace
+}
 #endif  // __LOGGER_H__
 
