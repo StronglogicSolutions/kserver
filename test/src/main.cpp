40213a7388b4ef31171071d3810a82a5eb185041
logicp <adventurist@gmail.com>
2020-04-19 21:28:15 -0400

    using multiple files for tests added another test suppress some compiler warnings

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -1,11 +1,6 @@
 #include <gtest/gtest.h>
-#include <iostream>
-#include <codec/util.hpp>
-#include <server/kserver.hpp>
-
-using namespace FileUtils;
-using namespace KYO;
-
+#include "codec/util_test.hpp"
+#include "kserver/kserver_test.hpp"
 /**
  * KServerTestEnvironment
  *
@@ -23,8 +18,8 @@ class KServerTestEnvironment {
   private:
   void setUp() {
     int arg_num = 3;
-    char* char_args[3] = {"127.0.0.1", "9876", "argument_string"};
-    argv = char_args;
+    const char* char_args[3] = {"127.0.0.1", "9876", "argument_string"};
+    argv = const_cast<char**>(char_args);
     argc = arg_num;
   }
 
@@ -35,35 +30,10 @@ class KServerTestEnvironment {
 /**
  * TESTING GLOBALS
  */
-auto util_cwd = get_executable_cwd();
-auto cwd = std::string{util_cwd.begin(), util_cwd.end() - 4};
-KServerTestEnvironment* ktest_env;
 
-/****************************************************
- *************** TEST SUITE *************************
- ****************************************************/
 
-/**
- * FileUtils::readEnvFile
- */
-TEST(KUtilities, readEnvFileTest) {
-  auto env_file_path = cwd + "/mock_data/mock_v.env";
-  std::cout << cwd << std::endl;
-  EXPECT_NE("", readEnvFile(env_file_path));
-}
-
-/**
- * KServer instantiation test
- */
-TEST(KServer, instantiateKServerTest) {
-  char* argv[3] = {"127.0.0.1", "9876", "argument_string"};
-  int argc = 3;
-  KServer kserver{std::move(argc), std::move(argv)};
-  EXPECT_NE(nullptr, &kserver);
-}
 
 int main(int argc, char** argv) {
-  ktest_env = new KServerTestEnvironment{};
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
