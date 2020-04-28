b271bc863772b7971766faa25fdb8e532b92ef4c
logicp <adventurist@gmail.com>
2020-04-28 19:24:45 -0400

    fixing typo

--- include/config/config_parser.hpp
+++ include/config/config_parser.hpp
@@ -2,6 +2,7 @@
 #define __CONFIG_PARSER_HPP__
 
 #include <string>
+
 #include "INIReader.h"
 
 namespace ConfigParser {
@@ -27,33 +28,35 @@ bool init() {
   return reader.ParseError() == 0;
 }
 
-bool is_initialized() {
-  return reader_ptr != nullptr;
-}
+bool is_initialized() { return reader_ptr != nullptr; }
 
 namespace Logging {
-  std::string level() {
-    return reader.Get("logging", "level", "info");
-  }
-  std::string path() {
-    return reader.Get("logging", "path", requiredConfig("[logging] path"));
-  }
+std::string level() { return reader.Get("logging", "level", "info"); }
+std::string path() {
+  return reader.Get("logging", "path", requiredConfig("[logging] path"));
 }
+}  // namespace Logging
 
 namespace Database {
 std::string pass() {
-  return reader.Get("database", "password", requiredConfig("[database] password"));;
+  return reader.Get("database", "password",
+                    requiredConfig("[database] password"));
 }
 
-std::string name() { return reader.Get("database", "name", requiredConfig("[database] name")); }
-
-std::string user() { return reader.Get("database", "user", requiredConfig("[database] user")); }
+std::string name() {
+  return reader.Get("database", "name", requiredConfig("[database] name"));
 }
+
+std::string user() {
+  return reader.Get("database", "user", requiredConfig("[database] user"));
+}
+}  // namespace Database
 
 namespace Process {
 std::string executor() {
-  return reader.Get("process", "executor", requiredConfig("[process] executor"));
+  return reader.Get("process", "executor",
+                    requiredConfig("[process] executor"));
 }
-}
+}  // namespace Process
 }  // namespace ConfigParser
 #endif  // __CONFIG_PARSER_HPP__
