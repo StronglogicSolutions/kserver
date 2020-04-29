ea990b79b1fdf8a07e80bbb8483abd5659015bc7
logicp <adventurist@gmail.com>
2020-04-28 21:14:25 -0400

    removing enumerated template types

--- config/db/queries.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-   s.id, s.mask, s.time,
-   a.name AS application,
-   (SELECT string_agg(name, ',') AS files FROM file WHERE sid = s.id)
-FROM schedule s
-INNER JOIN apps a
-ON a.mask = s.mask;
