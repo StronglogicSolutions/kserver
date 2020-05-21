79ea014320ce0d583817d86b570c0170d03c5b87
logicp <adventurist@gmail.com>
2020-05-21 16:55:44 -0400

    adding parameter to build debug version of kserver

--- build.sh
+++ build.sh
@@ -5,5 +5,9 @@ if [[ -z "$BUILD_ENV" ]]; then
   BUILD_ENV="LOCAL"
 fi
 
-BUILD_ENV=${BUILD_ENV} cmake . && make -j8
+if [[ -z "BUILD_TYPE" ]]; then
+  BUILD_TYPE="Release"
+fi
+
+BUILD_ENV=${BUILD_ENV} cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE . && make -j8
 
