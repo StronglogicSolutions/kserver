bd821275fe2a878cd6541d857c8fa533559a480e
logicp <adventurist@gmail.com>
2020-07-07 00:10:10 -0400

    ability to pass debug flag to cmake

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 BUILD_ENV=$1
-
+# BUILD_TYPE=$2
 if [[ -z "$BUILD_ENV" ]]; then
   BUILD_ENV="LOCAL"
 fi
 
-BUILD_ENV=${BUILD_ENV} cmake . && make -j8
+if [[ -z "$BUILD_TYPE" ]]; then
+  BUILD_TYPE="RELEASE"
+fi
+
+BUILD_ENV=${BUILD_ENV} cmake -DCMAKE_BUILD_TYPE=${BUILD_TYPE} . && make -j8
 
