d6f6abffd4392f1ac574d76671eda1bd7dcf2bd9
logicp <adventurist@gmail.com>
2020-08-30 00:58:31 -0400

    adding socket listener test

---
+++ test/src/socket/socket_listener.hpp
@@ -0,0 +1,97 @@
+#ifndef __KSERVER_TEST_HPP__
+#define __KSERVER_TEST_HPP__
+
+#include <gtest/gtest.h>
+
+#include <interface/socket_listener.hpp>
+// #include <task/task_queue.hpp>
+
+#include "../server/client/client.hpp"
+
+#include <thread>
+#include <chrono>
+
+const constexpr char* SERVER_TEST_PORT_ARG  = "--port=9876";
+const constexpr char* SERVER_TEST_IP_ARG    = "--ip=127.0.0.1";
+const constexpr char* SERVER_TEST_ARG       = "argument_string";
+const constexpr char* SERVER_TEST_ARGS[]    = {
+      SERVER_TEST_IP_ARG,
+      SERVER_TEST_PORT_ARG,
+      SERVER_TEST_ARG
+};
+const           int   SERVER_TEST_ARGC      = 3;
+
+SocketListener*    g_listener;
+MockClient*        g_client;
+
+void runClient() {
+  try {
+    auto raw_mode = true;
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+    MockClient client{raw_mode};
+    g_client = &client;
+    std::cout << "Client instantiated" << std::endl;
+    client.start();
+  } catch (const std::exception& e) {
+    std::cout << e.what() << std::endl;
+  }
+  std::cout << "runClient will exit" << std::endl;
+}
+
+// void runServer() {
+//   int argc = 3;
+//   bool test_mode_enabled = true;
+//   KServer kserver{argc, std::move(const_cast<char**>(SERVER_TEST_ARGS))};
+//   g_kserver = &kserver;
+//   kserver.set_handler(std::move(Request::RequestHandler{}));
+//   std::cout << "KServer set handler" << std::endl;
+//   kserver.init(test_mode_enabled);
+//   std::cout << "KServer initialized" << std::endl;
+//   kserver.run();
+// }
+
+void runServer() {
+  auto test_mode = true;
+  SocketListener socket_listener{SERVER_TEST_ARGC, const_cast<char**>(SERVER_TEST_ARGS)};
+  g_listener = &socket_listener;
+  socket_listener.init(test_mode);
+  socket_listener.run();
+}
+
+
+TEST(KServerTest, StartAndStopSession) {
+
+  std::thread server_thread{runServer};
+  std::this_thread::sleep_for(std::chrono::milliseconds(300));
+  std::thread client_thread{runClient};
+  // std::string received_bytes{};
+  // std::string op{};
+
+  while (g_client == nullptr || g_listener == nullptr)
+    ;
+
+  for (uint8_t i = 0; i < 100; i++) { // Stress test
+    g_client->sendMessage();
+    std::cout << "Client sent message" << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  }
+
+  // g_client->sendMessage();
+
+  std::this_thread::sleep_for(std::chrono::milliseconds(300));
+
+  g_client->close();
+
+  if (client_thread.joinable()) {
+    client_thread.join();
+  }
+
+  if (server_thread.joinable()) {
+    server_thread.join();
+  }
+
+  EXPECT_EQ(true, true);
+}
+
+
+#endif  // __KSERVER_TEST_HPP__
