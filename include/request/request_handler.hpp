a77b9c9ab4c90cb59315b68c24e57d2dc9302b4d
logicp <adventurist@gmail.com>
2020-12-11 22:18:31 -0500

    slightly more reasonable

--- include/request/request_handler.hpp
+++ include/request/request_handler.hpp
@@ -268,7 +268,7 @@ class RequestHandler {
       if (jobs.empty()) {
         KLOG("Cron - There are currently the following cron jobs: \n {}", jobs);
       }
-      std::this_thread::sleep_for(std::chrono::seconds(10));
+      std::this_thread::sleep_for(std::chrono::seconds(20));
     }
   }
 
