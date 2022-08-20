306d46354a6e1fead623dfb5df83e29a299867f2
logicp <logicp@logicp.ca>
2022-08-19 21:15:46 -0400

    dramatic refactor. partially complete

--- include/system/process/ipc/client/client.hpp
+++ include/system/process/ipc/client/client.hpp
@@ -1,174 +1,46 @@
 #pragma once
 
-#include <system/process/ipc/ipc.hpp>
 #include <zmq.hpp>
-#include <memory>
+#include <future>
+#include <system/process/ipc/ipc.hpp>
+#include "log/logger.h"
 
 namespace kiq {
-const std::string REQ_ADDRESS{"tcp://0.0.0.0:28473"};
-const std::string REP_ADDRESS{"tcp://0.0.0.0:28474"};
-const int32_t     POLLTIMEOUT{50};
-
-static const bool HasRequest(uint8_t mask)
-{
-  return (mask & 0x01 << 0);
-}
-
-static const bool HasReply(uint8_t mask)
-{
-  return (mask & 0x01 << 1);
-}
+const std::string  REQ_ADDRESS{"tcp://localhost:28473"};
+const std::string  REP_ADDRESS{"tcp://0.0.0.0:28474"};
+static const char* BACKEND_ADDRESS{"inproc://backend"};
+const int32_t      POLLTIMEOUT{50};
 
 static auto IsKeepAlive = [](auto type) { return type == ::constants::IPC_KEEPALIVE_TYPE; };
 
-struct IPC_Session
-{
-uint32_t    id;
-std::string name;
-};
-
-class IPCClient
+class IPCBrokerInterface
 {
 public:
-using u_ipc_msg_ptr = ipc_message::u_ipc_msg_ptr;
+  virtual ~IPCBrokerInterface() = default;
+  virtual void process_message(ipc_message::u_ipc_msg_ptr) = 0;
+};
 
-explicit IPCClient(uint32_t port)
-: m_context{1},
-  m_rep_socket{m_context, ZMQ_REP},
-  m_req_socket{m_context, ZMQ_REQ}
+class IPCWorker
 {
-  ResetSocket();
-}
-
-void ResetSocket(bool server = true)
-{
-  m_req_ready  = true;
-  m_req_socket = zmq::socket_t{m_context, ZMQ_REQ};
-  m_req_socket.setsockopt(ZMQ_LINGER, 0);
-  m_req_socket.connect(REQ_ADDRESS);
-  if (server)
-  {
-    m_rep_socket = zmq::socket_t{m_context, ZMQ_REP};
-    m_rep_socket.setsockopt(ZMQ_LINGER, 0);
-    m_rep_socket.bind(REP_ADDRESS);
-  }
-}
-
-bool SendIPCMessage(u_ipc_msg_ptr message, const bool use_req = false)
-{
-  auto           payload   = message->data();
-  int32_t        frame_num = payload.size();
-  zmq::socket_t& socket    = (use_req) ? m_req_socket : m_rep_socket;
-
-  for (int i = 0; i < frame_num; i++)
-  {
-    int  flag  = (i == (frame_num - 1)) ? 0 : ZMQ_SNDMORE;
-    auto data  = payload.at(i);
-    zmq::message_t message{data.size()};
-    std::memcpy(message.data(), data.data(), data.size());
-
-    socket.send(message, flag);
-  }
-
-  return true;
-}
-
-bool ReplyIPC()
-{
-  return SendIPCMessage(std::move(std::make_unique<okay_message>()));
-}
-
-bool ReceiveIPCMessage(bool use_req = true)
-{
-  using buffer_vector_t = std::vector<ipc_message::byte_buffer>;
-  buffer_vector_t received_message{};
-  zmq::message_t  message;
-  int             more_flag{1};
-  zmq::socket_t&  socket = (use_req) ? m_req_socket : m_rep_socket;
-
-  while (more_flag)
-  {
-    socket.recv(&message, static_cast<int>(zmq::recv_flags::none));
-    size_t size = sizeof(more_flag);
-    socket.getsockopt(ZMQ_RCVMORE, &more_flag, &size);
-
-    received_message.push_back(std::vector<unsigned char>{
-      static_cast<char*>(message.data()), static_cast<char*>(message.data()) + message.size()});
-  }
-
-  ipc_message::u_ipc_msg_ptr ipc_message = DeserializeIPCMessage(std::move(received_message));
-
-  if (ipc_message != nullptr)
-  {
-    if (IsKeepAlive(ipc_message->type())) Enqueue(std::make_unique<keepalive>());
-    m_rx_msgs.emplace_back(std::move(ipc_message));
-    m_req_ready = (use_req) ? true : m_req_ready;
-
-    return true;
-  }
-
-  ELOG("Client failed to receive message");
-  return false;
-}
-
-uint8_t Poll()
-{
-  uint8_t        poll_mask      = 0x00;
-  void*          rep_socket_ptr = static_cast<void*>(m_rep_socket);
-  void*          req_socket_ptr = static_cast<void*>(m_req_socket);
-  zmq_pollitem_t items[2]       = {{rep_socket_ptr, 0, ZMQ_POLLIN, 0},
-                                   {req_socket_ptr, 0, ZMQ_POLLIN, 0}};
-  zmq::poll(&items[0], 2, POLLTIMEOUT);
-  if (items[0].revents & ZMQ_POLLIN) poll_mask |= (0x01U << 0x00U);
-  if (items[1].revents & ZMQ_POLLIN) poll_mask |= (0x01U << 0x01U);
-
-  return poll_mask;
-}
-
-std::vector<u_ipc_msg_ptr> GetMessages()
-{
-  return std::move(m_rx_msgs);
-}
-
-void Shutdown() {
-  // TODO: handle shutdown
-}
-
-void ProcessQueue()
-{
-  const bool IS_REQUEST{true};
-  if (!m_outgoing_queue.empty() && m_req_ready)
-  {
-    m_req_ready = false;
-    SendIPCMessage(std::move(m_outgoing_queue.front()), IS_REQUEST);
-    m_outgoing_queue.pop_front();
-  }
-}
-
-void Enqueue(u_ipc_msg_ptr message)
-{
-  m_outgoing_queue.emplace_back(std::move(message));
-}
-
-bool HasOutbound() const
-{
-  return m_outgoing_queue.size() > 0;
-}
-
-void KeepAlive()
-{
-  Enqueue(std::make_unique<keepalive>());
-}
+using u_ipc_msg_ptr         = ipc_message::u_ipc_msg_ptr;
+public:
+  IPCWorker(zmq::context_t& ctx, std::string_view target_id, IPCBrokerInterface* broker);
+  void               start();
+  std::future<void>& stop();
+  void               send_ipc_message(u_ipc_msg_ptr message, bool tx = true);
 
 private:
-zmq::context_t                 m_context;
-zmq::socket_t                  m_rep_socket;
-zmq::socket_t                  m_req_socket;
-std::vector<u_ipc_msg_ptr>     m_tx_msgs;
-std::vector<u_ipc_msg_ptr>     m_rx_msgs;
-std::string                    m_addr;
-std::string                    m_rx_msg;
-std::deque<u_ipc_msg_ptr>      m_outgoing_queue;
-bool                           m_req_ready;
+  void run();
+  void recv();
+
+  IPCBrokerInterface* manager_;
+  zmq::context_t&     ctx_;
+  zmq::socket_t       backend_;
+  zmq::socket_t       tx_sink_;
+  std::string_view    target_;
+  std::string         name_;
+  bool                active_;
+  std::promise<void>  promise_;
+  std::future<void>   future_;
 };
 } // ns kiq
--- include/system/process/ipc/manager/manager.hpp
+++ include/system/process/ipc/manager/manager.hpp
@@ -1,154 +1,56 @@
 #pragma once
 
-#include <thread>
-#include <deque>
-#include <condition_variable>
-#include <mutex>
-
-#include "log/logger.h"
 #include "interface/worker_interface.hpp"
 #include "server/types.hpp"
-#include "system/process/ipc/ipc.hpp"
 #include "system/process/ipc/client/client.hpp"
 
 namespace kiq {
 using u_ipc_msg_ptr         = ipc_message::u_ipc_msg_ptr;
 using SystemCallback_fn_ptr = std::function<void(int32_t, const std::vector<std::string>&)>;
-using SystemDispatch_t      = std::function<void(std::deque<u_ipc_msg_ptr>::const_iterator)>;
+using SystemDispatch_t      = std::function<void(u_ipc_msg_ptr)>;
+using Payload               = std::vector<std::string>;
+
+static auto GetPayload = [](auto m) { return Payload{m->platform(), m->id(),   m->user(), "", m->content(), m->urls(), std::to_string(m->repost()), m->args()}; };
+static auto GetInfo    = [](auto m) { return Payload{m->platform(), m->type(), m->info()};                                                                      };
+static auto GetError   = [](auto m) { return Payload{m->name(),     m->id(),   m->user(), m->error(), ""};                                                      };
+static auto GetRequest = [](auto m) { return Payload{m->platform(), m->id(),   m->user(), m->content(), m->args()};                                             };
+
 static const uint32_t DEFAULT_PORT{static_cast<uint32_t>(std::stoul(config::Process::ipc_port()))};
 
 static auto NOOP = [] { (void)(0); };
 
-class IPCManager : public Worker {
+class IPCManager : public IPCBrokerInterface
+{
 public:
 
-IPCManager(SystemCallback_fn_ptr system_event_fn)
-: m_system_event_fn(system_event_fn),
-  m_req_ready(true)
-{
-  m_clients.insert(std::pair<int32_t, IPCClient>{ALL_CLIENTS, IPCClient{DEFAULT_PORT}});
-  m_clients.at(ALL_CLIENTS).KeepAlive();
-}
+IPCManager(SystemCallback_fn_ptr system_event_fn);
+~IPCManager() final;
+void process(std::string message, int32_t fd);
+bool ReceiveEvent(int32_t event, const std::vector<std::string> args);
+void close(int32_t fd);
+void process_message(u_ipc_msg_ptr msg);
+void start();
 
-void process(std::string message, int32_t fd)
-{
-  KLOG("Received outgoing IPC request");
-  m_clients.at(ALL_CLIENTS).Enqueue(std::move(std::make_unique<kiq_message>(message)));
-}
+private:
+  void loop();;
 
-bool ReceiveEvent(int32_t event, const std::vector<std::string> args)
-{
-  auto Deserialize = [](auto args)
-  {
-    return std::make_unique<platform_message>(
-      args.at(constants::PLATFORM_PAYLOAD_PLATFORM_INDEX),       args.at(constants::PLATFORM_PAYLOAD_ID_INDEX),
-      args.at(constants::PLATFORM_PAYLOAD_USER_INDEX),           args.at(constants::PLATFORM_PAYLOAD_CONTENT_INDEX),
-      args.at(constants::PLATFORM_PAYLOAD_URL_INDEX),            args.at(constants::PLATFORM_PAYLOAD_REPOST_INDEX) == "y",
-      std::stoi(args.at(constants::PLATFORM_PAYLOAD_CMD_INDEX)), args.at(constants::PLATFORM_PAYLOAD_ARGS_INDEX));
-  };
-
-  KLOG("Processing IPC message for event {}", event);
-  bool received{true};
-
-  if (event == SYSTEM_EVENTS__PLATFORM_POST_REQUESTED || event == SYSTEM_EVENTS__PLATFORM_EVENT)
-    m_clients.at(ALL_CLIENTS).Enqueue(std::move(Deserialize(args)));
-  else
-    received = false;
-
-  return received;
-}
-
-void close(int32_t fd)
-{
-  std::unordered_map<int32_t, IPCClient>::iterator it = m_clients.find(fd);
-
-  if (it != m_clients.end())
-  {
-    IPCClient& client = it->second;
-    client.Shutdown();
-    m_clients.erase(it);
-  }
-}
-
-void HandleClientMessages()
-{
-  using ipc_msg_it_t = std::deque<u_ipc_msg_ptr>::iterator;
-  using Payload      = std::vector<std::string>;
-
-  auto GetPayload = [](auto m) { return Payload{m->platform(), m->id(),   m->user(), "", m->content(), m->urls(), std::to_string(m->repost()), m->args()}; };
-  auto GetInfo    = [](auto m) { return Payload{m->platform(), m->type(), m->info()};                                                                      };
-  auto GetError   = [](auto m) { return Payload{m->name(),     m->id(),   m->user(), m->error(), ""};                                                      };
-  auto GetRequest = [](auto m) { return Payload{m->platform(), m->id(),   m->user(), m->content(), m->args()};                                             };
-
-  std::map<uint8_t, SystemDispatch_t> dispatch_table{
-  {::constants::IPC_PLATFORM_TYPE   , [&, this](auto it) { m_system_event_fn(::constants::IPC_PLATFORM_TYPE,    GetPayload(static_cast<platform_message*>(it->get()))); }},
-  {::constants::IPC_PLATFORM_REQUEST, [&, this](auto it) { m_system_event_fn(::constants::IPC_PLATFORM_REQUEST, GetRequest(static_cast<platform_request*>(it->get()))); }},
-  {::constants::IPC_PLATFORM_INFO   , [&, this](auto it) { m_system_event_fn(::constants::IPC_PLATFORM_INFO,    GetInfo   (static_cast<platform_info*>   (it->get()))); }},
-  {::constants::IPC_PLATFORM_ERROR  , [&, this](auto it) { m_system_event_fn(::constants::IPC_PLATFORM_ERROR,   GetError  (static_cast<platform_error*>  (it->get()))); }},
+  std::map<uint8_t, SystemDispatch_t> m_dispatch_table{
+  {::constants::IPC_PLATFORM_TYPE   , [&, this](auto it) { m_system_event_fn(SYSTEM_EVENTS__PLATFORM_NEW_POST, GetPayload(static_cast<platform_message*>(it.get()))); }},
+  {::constants::IPC_PLATFORM_REQUEST, [&, this](auto it) { m_system_event_fn(SYSTEM_EVENTS__PLATFORM_REQUEST,  GetRequest(static_cast<platform_request*>(it.get()))); }},
+  {::constants::IPC_PLATFORM_INFO   , [&, this](auto it) { m_system_event_fn(SYSTEM_EVENTS__PLATFORM_INFO,     GetInfo   (static_cast<platform_info*>   (it.get()))); }},
+  {::constants::IPC_PLATFORM_ERROR  , [&, this](auto it) { m_system_event_fn(SYSTEM_EVENTS__PLATFORM_ERROR,    GetError  (static_cast<platform_error*>  (it.get()))); }},
   {::constants::IPC_KEEPALIVE_TYPE  , [&, this](auto it) { m_daemon.reset();                                                                                            }},
   {::constants::IPC_OK_TYPE         , [&, this](auto it) { NOOP();                                                                                                      }}};
 
-  for (ipc_msg_it_t it = m_incoming_queue.begin(); it != m_incoming_queue.end();)
-  {
-    dispatch_table[it->get()->type()](it);
-    it = m_incoming_queue.erase(it);
-  }
-}
-
-private:
-virtual void loop() override
-{
-  uint8_t mask;
-  while (m_is_running)
-  {
-    for (auto&& [fd, client] : m_clients)
-    {
-      try
-      {
-        {
-          std::unique_lock<std::mutex> lock{m_mutex};
-          mask = client.Poll();
-        }
-
-        if (HasRequest(mask) && client.ReceiveIPCMessage(false))
-          client.ReplyIPC();
-
-        if (HasReply(mask))
-          client.ReceiveIPCMessage();
-      }
-      catch (const std::exception& e)
-      {
-        ELOG("Caught IPC exception: {}\nResetting client socket", e.what());
-        client.ResetSocket(false);
-      }
-
-      std::vector<u_ipc_msg_ptr> messages = client.GetMessages();
-
-      m_incoming_queue.insert(m_incoming_queue.end(), std::make_move_iterator(messages.begin()),
-                                                      std::make_move_iterator(messages.end())  );
-      if (client.HasOutbound() && !m_daemon.active())
-        m_daemon.reset();
-
-      client.ProcessQueue();
-    }
-
-    HandleClientMessages();
-
-    if (!m_daemon.validate())
-    {
-      m_daemon.stop();
-      ELOG("IPC session is unreliable");
-      m_clients.at(ALL_CLIENTS).ResetSocket(false);
-      m_clients.at(ALL_CLIENTS).KeepAlive();
-    }
-  }
-}
-
-std::unordered_map<int32_t, IPCClient> m_clients;
-SystemCallback_fn_ptr                  m_system_event_fn;
-std::deque<u_ipc_msg_ptr>              m_incoming_queue;
-std::mutex                             m_mutex;
-bool                                   m_req_ready;
-session_daemon                         m_daemon;
+  std::unordered_map<int32_t, IPCWorker> m_clients;
+  SystemCallback_fn_ptr                  m_system_event_fn;
+  // std::deque<u_ipc_msg_ptr>              m_incoming_queue;
+  std::mutex                             m_mutex;
+  bool                                   m_req_ready;
+  session_daemon                         m_daemon;
+  zmq::context_t                         m_context;
+  zmq::socket_t                          m_public_;
+  zmq::socket_t                          m_backend_;
+  std::future<void>                      m_future;
 };
 } // ns kiq
