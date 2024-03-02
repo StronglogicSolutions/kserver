0e8ffcbf6dc1f45e225444d9949577c97868cecd
Emmanuel Buckshi <adventurist@gmail.com>
2024-03-01 21:15:35 -0500

    returning a default broker

--- include/system/process/ipc/manager.cpp
+++ include/system/process/ipc/manager.cpp
@@ -17,12 +17,15 @@ namespace kiq
     kygui_peer
   };
 
-  static std::string_view find_peer(const std::string& s)
+  static std::string_view find_peer(const std::string& s, bool get_default = false)
   {
     for (const auto& peer : ipc_peers)
 
       if (s.find(peer) != std::string::npos)
         return peer;
+      else
+      if (get_default)
+        return ipc_peers.front();
     return "";
   }
   //*******************************************************************//
